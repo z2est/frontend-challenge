b2f7aac60e11f5a1e7cf4ca8935d930882b76665
ChrisPark <namegpark@naver.com>
2021-06-30 21:22:26 +0900

    Challenge - Exam1 메뉴 관련 이벤트 리스너 추가

--- Challenge1/js/main.js
+++ Challenge1/js/main.js
@@ -1,10 +1,28 @@
 "use strict";
 
 window.onload = function() {
+    
     const navbarMenu = document.querySelectorAll(".navbar__menu li");
-    if(navbarMenu.length > 0){
-        navbarMenu.addEventListener("click", function() {
-            this.add
-        });
+    const menuLength = navbarMenu.length;
+    console.log(navbarMenu);
+    
+    if(menuLength > 0){
+        
+        for(let obj=0; obj < menuLength; obj++){
+            console.log("mememem");
+            navbarMenu[obj].addEventListener("click", function() {
+                /**
+                 * 기존 selected가 있었던 것을 지운다.
+                 */
+                for(let i=0; i<menuLength; i++){
+                navbarMenu[i].classList.remove("selected");
+                }
+                
+                // selected class가 모두 사라졌으므로 선택된 태그에 selected를 추가한다.
+                this.classList.add("selected");
+                
+            });
+        }
     }
 }
+
